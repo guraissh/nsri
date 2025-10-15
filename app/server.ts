120d024866711039135630f15f2bcfefec78263d
grs <guraish@proton.me>
2025-10-15 23:22:45 +0100

    react-router-hono-server

---
+++ app/server.ts
@@ -0,0 +1,4 @@
+// app/server.ts
+import { createHonoServer } from "react-router-hono-server/node";
+
+export default createHonoServer({/* options */});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev"; // add this
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouterHonoServer({ runtime: 'bun' }), // add this
+    tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: { external: ['@vidstack/react', '@vidstack/react/player', '@vidstack/react/media'] },
-  server: {cors: true},
+  server: { cors: true },
   logLevel: 'info'
 });
